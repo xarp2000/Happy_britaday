6e2d83657b87ee3c441e8df44c66f18d96a411bc
xarp2000 <138525203+xarp2000@users.noreply.github.com>
2023-11-27 17:16:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function newImage()
+{
+	
+ fabric.Image.fromURL('BirthdayImage.jpg' , function(Img){
+ blockImageObject = Img;
+ 
+ blockImageObject.scaleToWidth(700);
+ blockImageObject.scaleToHeight(510);
+ blockImageObject.set({
+   top:0,
+   left:0
+    })
+    canvas.add(blockImageObject);
+    })	
+}
+
+function playSound(){
+	x.play();
+}
